81c6b173b424035a7706df05fffe043190bee148
apiwatc <42307002+apiwatc@users.noreply.github.com>
2019-08-23 11:43:13 -0700

    Update CollatzSquare.py

--- CollatzSquare.py
+++ CollatzSquare.py
@@ -1,3 +1,10 @@
+"""
+If number is even, then collatz() should print number // 2 and return this value. 
+If number is odd, then collatz() should print and return 3 * number + 1.
+Let the user type in an integer and that keeps calling collatz() on that number 
+until the function returns the value 1. (Amazingly enough, this sequence actually 
+works for any integer—sooner or later, using this sequence, you’ll arrive at 1! 
+"""
 def collatez(number):
     while number > 1:
         print(number)
